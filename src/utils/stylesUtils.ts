47be9b2134f413ce799054613dfcfe69046dfea1
ChronicStone <cypthao@gmail.com>
2021-12-12 16:15:09 +0100

    feat: field props mapper

--- src/utils/stylesUtils.ts
+++ src/utils/stylesUtils.ts
@@ -1,4 +1,4 @@
-import { gridMap } from "./twClasses"
+import { gridMap } from "../constants/twClasses"
 type TailwindBreakpoint = '' | 'sm:' | 'md:' | 'lg:' | 'xl:'
 type TailwindSizeCtrl = 'grid-cols' | 'col-span'
 type TailwindSizeList = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12'
