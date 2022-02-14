9812fecb770eeed81f56e4e203f9ecc98c62d839
ChronicStone <cypthao@gmail.com>
2022-02-14 17:32:50 +0100

    feat: render fct support on description popup

--- src/components/DescriptionPopup.vue
+++ src/components/DescriptionPopup.vue
@@ -3,27 +3,33 @@
     <NModal to="#sweetforms__modalContainer" v-model:show="showDescription">
         <NCard closable :on-close="CloseModal" style="width: 600px;z-index: 30000;" title="Modal" :bordered="false" size="huge">
             <template #header>
-                {{description?.title ?? fieldLabel}}
+                <PopupLabel/>
             </template>
             <NScrollbar style="max-height: 80vh;" class="pr-6">
-                <div v-html="description?.content ?? description" />
+                <PopupContent/>
             </NScrollbar>
         </NCard>
     </NModal>
 </template>
 
-<script setup lang="ts">
-import { ref, defineProps, inject, watch } from 'vue';
+<script setup lang="tsx">
+import { ref, defineProps, inject, watch, VNodeChild } from 'vue';
 import { NModal, NCard , NScrollbar } from "naive-ui"
 import { ModalOverlayInjectKey  } from "@/constants/injectionKeys";
+import { render } from "../utils"
 
-const showDescription = ref(false)
 const props =  defineProps({
     fieldLabel: String,
-    description:  [String, Object]
+    description:  [String, Object, Function]
 })
 
+const showDescription = ref(false)
 const CloseModal = () =>  showDescription.value = false
+
+const PreRenderStringContent = (content: string | VNodeChild) => typeof content === 'string' ? () => (<div v-html={content}></div>) : content
+
+const PopupLabel = () => render(props.description?.title ?? props.fieldLabel)
+const PopupContent = () => render(PreRenderStringContent(props.description?.content ?? props.description))
 
 const modalOverlay: { show: () => void, hide: () => void } | undefined = inject(ModalOverlayInjectKey)
 watch(() => showDescription.value, (value) => {
